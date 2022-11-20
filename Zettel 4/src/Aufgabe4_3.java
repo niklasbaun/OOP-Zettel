9a2ad380bb69e5ffdbc650c19f335afd4b575752
Niklas Baun <niklasbaun@gmail.com>
2022-11-20 15:13:57 +0100

    fixed error in 4_2 to fix calculation start with 4_3 still not sure what each var means

--- Zettel 4/src/Aufgabe4_3.java
+++ Zettel 4/src/Aufgabe4_3.java
@@ -1,8 +1,15 @@
 public class Aufgabe4_3 {
     public static void main(String[] args) {
-
+        krt(4, 2, 0.5);
     }
 
+    /**
+     * calls the recursiv method to calculate the nth root of a given number with Herons algorithm
+     * @param a number to calculate the root of
+     * @param k nth root
+     * @param d precision
+     * @return the nth root of a given number
+     */
     static double krt(double a, double k, double d) {
         if (a <= 0){
             return 0;
@@ -11,11 +18,51 @@ public class Aufgabe4_3 {
         }
     }
 
+    /**
+     * calculates the nth root of a given number with Herons algorithm
+     * @param a number to calculate the root of
+     * @param k nth root
+     * @param d precision
+     * @param x_n current guess
+     * @return the nth root of a given number
+     */
     static double krtH(double a, double k, double d, double x_n) {
         if (d == 0) {
             return a;
+        } else if((x_n-(x_n-1) < d) ) {
+            return x_n;
         } else {
-            return (1/k)*((k-1)+d+(1/krtH(a, k, d++, d * x_n)));
+            return krtH(a, k, d, (1 / k) * ((k - 1) * x_n + a / Math.pow(x_n, k - 1)));
+        }
+    }
+
+    /**
+     * tests the method krt with different values
+     */
+    static void testKrt(){
+        double test1Value = krt(2, 2, 0.0001);
+        if (test1Value == 1.4142){
+            System.out.println("Test 1 passed");
+        }else{
+            System.out.println("Test 1 failed");
+            System.out.println("Expected: 1.4142");
+            System.out.println("Actual: " + test1Value);
+        }
+        double test2Value = krt(2, 3, 0.0001);
+        if (test2Value == 1.2599){
+            System.out.println("Test 2 passed");
+        }else{
+            System.out.println("Test 2 failed");
+            System.out.println("Expected: 1.2599");
+            System.out.println("Actual: " + test2Value);
+        }
+        double test3Value = krt(2, 4, 0.0001);
+        if (test3Value == 1.1892){
+            System.out.println("Test 3 passed");
+        }else{
+            System.out.println("Test 3 failed");
+            System.out.println("Expected: 1.1892");
+            System.out.println("Actual: " + test3Value);
         }
     }
 
