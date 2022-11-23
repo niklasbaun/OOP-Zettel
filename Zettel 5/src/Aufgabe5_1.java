411802dc95ffc1df0e1e2b0265c36cb5fdeb8df3
niklas <niklasbaun@gmail.com>
2022-11-23 13:36:52 +0100

    zettel 5 fixed errors in Aufgabe 2, added simple tests

--- Zettel 5/src/Aufgabe5_1.java
+++ Zettel 5/src/Aufgabe5_1.java
@@ -5,6 +5,7 @@ public class Aufgabe5_1 {
     }
 
     static void testAll(){
+        System.out.println("Tests for getMinimum");
         //test 1 for getMinimum
         float[] arrMin1 = {1, 2, 3, 4, 5};
         float min1 = 1;
@@ -12,8 +13,8 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + min1);
-            System.out.println("Actual Value" + getMinimum(arrMin1));
+            System.out.println("Expected Value " + min1);
+            System.out.println("Actual Value " + getMinimum(arrMin1));
         }
         //test 2 for getMinimum
         float[] arrMin2 = {4.5F, -1.2F, 5, 7.6F, 10};
@@ -22,20 +23,22 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + min2);
-            System.out.println("Actual Value" + getMinimum(arrMin2));
+            System.out.println("Expected Value " + min2);
+            System.out.println("Actual Value " + getMinimum(arrMin2));
         }
         //test 3 for getMinimum
-        float[] arrMin3 = {500, 1000, 2000, 3000, 1500};
+        float[] arrMin3 = {1000, 500, 2000, 3000, 1500};
         int min3 = 500;
         if (min3 == getMinimum(arrMin3)) {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + min3);
-            System.out.println("Actual Value" + getMinimum(arrMin3));
+            System.out.println("Expected Value " + min3);
+            System.out.println("Actual Value " + getMinimum(arrMin3));
         }
 
+        System.out.println("--------------------");
+        System.out.println("Tests for getMaximum");
         //test 1 for getMaximum
         float[] arrMax1 = {4.5F, 1.2F, 5, 7.3F};
         float max1 = 7.3F;
@@ -43,8 +46,8 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + max1);
-            System.out.println("Actual Value" + getMaximum(arrMax1));
+            System.out.println("Expected Value " + max1);
+            System.out.println("Actual Value " + getMaximum(arrMax1));
         }
         //test 2 for getMaximum
         float[] arrMax2 = {-44, -13, -25, -7};
@@ -53,8 +56,8 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + max2);
-            System.out.println("Actual Value" + getMaximum(arrMax2));
+            System.out.println("Expected Value " + max2);
+            System.out.println("Actual Value " + getMaximum(arrMax2));
         }
         //test 3 for getMaximum
         float[] arrMax3 = {500, 1000, 2000, 3000};
@@ -63,10 +66,12 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + max3);
-            System.out.println("Actual Value" + getMaximum(arrMax3));
+            System.out.println("Expected Value " + max3);
+            System.out.println("Actual Value " + getMaximum(arrMax3));
         }
 
+        System.out.println("--------------------");
+        System.out.println("Tests for getAverage");
         //test 1 for getAverage
         float[] arrAvg1 = {4, 1, 5, 6};
         float avg1 = 4;
@@ -74,8 +79,8 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + avg1);
-            System.out.println("Actual Value" + calculateAverage(arrAvg1));
+            System.out.println("Expected Value " + avg1);
+            System.out.println("Actual Value " + calculateAverage(arrAvg1));
         }
         //test 2 for getAverage
         float[] arrAvg2 = {4, 1, 5, 6, 10};
@@ -84,20 +89,22 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + avg2);
-            System.out.println("Actual Value" + calculateAverage(arrAvg2));
+            System.out.println("Expected Value " + avg2);
+            System.out.println("Actual Value " + calculateAverage(arrAvg2));
         }
         //test 3 for getAverage
-        float[] arrAvg3 = {-4.1F, -11.2F, -13, -16.2F, -10, -5.3F};
-        float avg3 = -8.3F;
+        float[] arrAvg3 = {-4.1F, -11.4F, -13, -16.2F, -10, -5.3F};
+        float avg3 = -10;
         if (avg3 == calculateAverage(arrAvg3)) {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + avg3);
-            System.out.println("Actual Value" + calculateAverage(arrAvg3));
+            System.out.println("Expected Value " + avg3);
+            System.out.println("Actual Value " + calculateAverage(arrAvg3));
         }
 
+        System.out.println("--------------------");
+        System.out.println("Tests for isSorted");
         //test 1 for isSorted
         float[] arrSort1 = {1, 2, 3, 4, 5};
         boolean sorted1 = true;
@@ -105,8 +112,8 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + sorted1);
-                System.out.println("Actual Value" + isSorted(arrSort1, true));
+            System.out.println("Expected Value " + sorted1);
+                System.out.println("Actual Value " + isSorted(arrSort1, true));
         }
 
         //test 2 for isSorted
@@ -116,8 +123,8 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + sorted2);
-            System.out.println("Actual Value" + isSorted(arrSort2, false));
+            System.out.println("Expected Value " + sorted2);
+            System.out.println("Actual Value " + isSorted(arrSort2, false));
         }
 
         //test 3 for isSorted
@@ -127,8 +134,8 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + sorted3);
-            System.out.println("Actual Value" + isSorted(arrSort3, true));
+            System.out.println("Expected Value " + sorted3);
+            System.out.println("Actual Value " + isSorted(arrSort3, true));
         }
 
         //test 4 for isSorted
@@ -138,8 +145,8 @@ public class Aufgabe5_1 {
             System.out.println("Test passed");
         } else {
             System.out.println("Test failed");
-            System.out.println("Expected Value" + sorted4);
-            System.out.println("Actual Value" + isSorted(arrSort4, true));
+            System.out.println("Expected Value " + sorted4);
+            System.out.println("Actual Value " + isSorted(arrSort4, true));
         }
     }
 
@@ -149,7 +156,7 @@ public class Aufgabe5_1 {
      * @return
      */
     static float getMinimum(float[] numbers) {
-        return getMinimumH(numbers, numbers.length);
+        return getMinimumH(numbers, numbers.length, numbers[0]);
     }
     /**
      * helper method for getMinimum
@@ -157,11 +164,14 @@ public class Aufgabe5_1 {
      * @param length
      * @return smallest number in the array
      */
-    static float getMinimumH(float[] numbers, int length) {
-        if (numbers.length == 1) {
-            return numbers[0];
+    static float getMinimumH(float[] numbers, int length, float min) {
+        if (length <= 0) {
+            return min;
         } else {
-            return Math.min(numbers[length], getMinimumH(numbers, length-1));
+            if (numbers[length-1] < min) {
+                min = numbers[length-1];
+            }
+            return getMinimumH(numbers, length - 1, min);
         }
     }
     /**
@@ -188,8 +198,8 @@ public class Aufgabe5_1 {
      */
     static float calculateAverage(float[] numbers){
         float sum = 0;
-        for (int i = 0; i < numbers.length; i++){
-            sum += numbers[i];
+        for (float num : numbers){
+            sum += num;
         }
         return sum / numbers.length;
     }
@@ -200,25 +210,33 @@ public class Aufgabe5_1 {
      * @return true if the array is sorted in ascending order, if in descending order false
      */
     static boolean isSorted(float[] numbers, boolean ascending){
-        float previous = numbers[0];
+
         //test if ascending
         if (ascending = true) {
-            for (float current : numbers) {
-                if (current < previous) {
-                    return false;
+            int j = 0;
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] > numbers[i - 1]) {
+                    j++;
                 }
-                previous = current;
             }
-            return true;
+            if (j == numbers.length - 1) {
+                return true;
+            } else {
+                return false;
+            }
         //test if descending
         } else {
-            for (float current : numbers) {
-                if (current > previous) {
-                    return false;
+            int j = 0;
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] < numbers[i - 1]) {
+                    j++;
                 }
-                previous = current;
             }
-            return true;
+            if (j == numbers.length - 1) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
     }
--- Zettel 5/src/Aufgabe5_2.java
+++ Zettel 5/src/Aufgabe5_2.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Aufgabe5_2 {
     public static void main(String[] args) {
-        testAll();
+        testAllGPS();
     }
 
-    static void testAll() {
+    static void testAllGPS() {
         double testArrayGPS[] = {-20.0, 0.0,200.0, -18.5, -0.647,200.577, -16.85, -1.237,
                 201.16, -15.035, -1.763, 201.739, -13.038, -2.219,
                 202.299, -10.842, -2.599, 202.824, -8.426, -2.894,
@@ -11,8 +13,12 @@ public class Aufgabe5_2 {
                 0.369, -3.182, 203.998, 3.861, -3.205, 203.85, 7.284,
                 -3.176, 203.469, 10.638, -3.209, 202.868, 13.926, -3.175,
                 202.06, 17.147, -3.209,201.059, 20, -3.174, 199.877};
+
         //test for distance
         double testdistance = 0.0;
+        System.out.println(distance(testArrayGPS) + "m");
+        System.out.println(velocity(testArrayGPS) + "m/s");
+        System.out.println(maxVelocity(testArrayGPS) + "m/s");
 
     }
 
@@ -24,13 +30,17 @@ public class Aufgabe5_2 {
     static double distance(double[] gps){
         //split the array into three arrays for x, y and z coordinates using the helper methods
         double[] x = getEveryThirdElement(gps);
+        System.out.println(Arrays.toString(x));
         gps = removeFirstElement(gps);
         double[] y = getEveryThirdElement(gps);
+        System.out.println(Arrays.toString(y));
         gps = removeFirstElement(gps);
         double[] z = getEveryThirdElement(gps);
-        //calculate the distance between the  each of the points
+        System.out.println(Arrays.toString(z));
+        //calculate the distance between each of the points
         double distance = 0;
-        for (int i = 0; i < x.length - 1; i++) {
+        //length of the array -2, because the array starts at 0, and we need to stop at the second last element
+        for (int i = 0; i < x.length - 2; i++) {
             distance += Math.sqrt(Math.pow(x[i + 1] - x[i], 2) + Math.pow(y[i + 1] - y[i], 2) + Math.pow(z[i + 1] - z[i], 2));
         }
         return distance;
@@ -42,16 +52,10 @@ public class Aufgabe5_2 {
      * @return velocity in m/s
      */
     static double velocity(double[] gps){
-        //split the array into three arrays for x, y and z coordinates using the helper methods
-        double[] x = getEveryThirdElement(gps);
-        gps = removeFirstElement(gps);
-        double[] y = getEveryThirdElement(gps);
-        gps = removeFirstElement(gps);
-        double[] z = getEveryThirdElement(gps);
         //calculate the velocity between the points
         //is length of the array - 1 because the first element is the start point
-        int time = x.length-1;
-        return distance(gps) / (time);
+        int time = gps.length / 3 - 1;
+        return distance(gps) / time;
     }
 
     /**
@@ -68,11 +72,12 @@ public class Aufgabe5_2 {
         double[] z = getEveryThirdElement(gps);
         //calculate the velocities between the single points
         double[] velocities = new double[x.length - 1];
-        //calcualte the velocities between the points
-        for (int i = 0; i < velocities.length; i++) {
+        //calculate the velocities between the points
+        for (int i = 0; i < velocities.length-1; i++) {
+            //velocity is equal to the distance between the points, because the time passed is 1 second
             velocities[i] = Math.sqrt(Math.pow(x[i + 1] - x[i], 2) + Math.pow(y[i + 1] - y[i], 2) + Math.pow(z[i + 1] - z[i], 2));
         }
-        //find the maximum velocity
+        //find the maximum velocity (value) in the array
         double maxVelocity = 0;
         for (int i = 1; i < velocities.length; i++) {
             if (velocities[i] > maxVelocity) {
@@ -99,7 +104,7 @@ public class Aufgabe5_2 {
         //find the start and end point
         int startPoint = 0;
         int endPoint = 0;
-        for (i = 0, i < x.length; i++) {
+        for (int i = 0; i < x.length; i++) {
             if (x[i] == start[0] && y[i] == start[1] && z[i] == start[2]) {
                 startPoint = i;
             }
@@ -110,10 +115,12 @@ public class Aufgabe5_2 {
         //create a new array with the coordinates between start and end
         double[] partialGPS = new double[(endPoint - startPoint) * 3];
         //fill the array with the coordinates
-        for (i = startPoint; int j = 0;i < endPoint; i++, j += 3) {
-            partialGPS[j] = x[i];
-            partialGPS[j + 1] = y[i];
-            partialGPS[j + 2] = z[i];
+        for (int s = startPoint;  s < endPoint; s++ ) {
+            int j = 0;
+            partialGPS[j] = x[s];
+            partialGPS[j + 1] = y[s];
+            partialGPS[j + 2] = z[s];
+            j += 3;
         }
         return partialGPS;
     }
@@ -124,9 +131,9 @@ public class Aufgabe5_2 {
      * @return array with every third element
      */
     static double[] getEveryThirdElement(double[] arr) {
-        double[] everyThirdElement = new double[arr.length / 3];
+        double[] everyThirdElement = new double[arr.length / 3 + 1];
         int j = 0;
-        for (int i = 2; i < arr.length; i += 3) {
+        for (int i = 0; i < arr.length; i += 3) {
             everyThirdElement[j] = arr[i];
             j++;
         }
