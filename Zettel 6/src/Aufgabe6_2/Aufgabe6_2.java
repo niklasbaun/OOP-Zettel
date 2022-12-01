ac3c75fa3162ffcbc257187869249e795422ed4a
Niklas Baun <niklasbaun@gmail.com>
2022-12-01 19:36:59 +0100

    added tests in main

--- Zettel 6/src/Aufgabe6_2/Aufgabe6_2.java
+++ Zettel 6/src/Aufgabe6_2/Aufgabe6_2.java
@@ -4,8 +4,12 @@ package Aufgabe6_2;
  */
 public class Aufgabe6_2 {
     public static void main(String[] args) {
-        Playfair playfair = new Playfair("Apfelstrudel");
-        System.out.println(playfair.printSquare());
-        System.out.println(playfair.encode("Es ist Mittwoch"));
+        Playfair playfair1= new Playfair("Apfelstrudel");
+        System.out.println(playfair1.printSquare()); //erwarteter Wert: A P F E L  S T R U D  E L B C G  H I K M N  O Q V W X
+        System.out.println(playfair1.encode("Es ist Mittwoch")); //erwarteter Wert: AUHTUIQLUQWEOS
+
+        Playfair playfair2= new Playfair("Codeword");
+        System.out.println(playfair2.printSquare()); //erwarteter Wert: C O D E W  R A B F G  H I K L M  N P Q S T  U V X Y Z
+        System.out.println(playfair2.encode("Hallo Welt")); //erwarteter Wert: HZLXWZLX
     }
 }
