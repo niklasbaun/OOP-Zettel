f85d4108d8b60b84d066636c8a5b238079ebe24a
niklas <niklasbaun@gmail.com>
2022-12-07 11:38:09 +0100

    added tests

--- Zettel 7/src/Aufgabe7_1.java
+++ Zettel 7/src/Aufgabe7_1.java
@@ -1,64 +1,127 @@
+/**
+ * @author Walter, Annika; Baun, Niklas; Mahlberg, Kilian
+ */
 public class Aufgabe7_1 {
-    private static int[] primesUpTo(int n) {
-        //check if n is a valid number
-        if (n < 0) {
-            System.out.println("n must be greater than 0");
-            return null;
-        }
-        //list all numbers up to n in array
-        int[] numbers = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            numbers[i] = i;
-        }
-        //remove 0 and 1 from the array
-        int[] numbersWithout0And1 = new int[n - 2];
-        for (int i = 0; i < n - 2; i++) {
-            numbersWithout0And1[i] = numbers[i + 2];
-        }
-        //remove all multiples of all numbers up to sqrt(n)+2 from the array
-        // by replacing all multiples with 0
-        int[] numbersWithoutMultiples = new int[n - 2];
-        int counter = 0;
-        for (int i = 0; i < n - 2; i++) {
-            if (numbersWithout0And1[i] != 0) {
-                numbersWithoutMultiples[counter] = numbersWithout0And1[i];
-                counter++;
-                for (int j = i + 1; j < n - 2; j++) {
-                    if (numbersWithout0And1[j] % numbersWithout0And1[i] == 0) {
-                        numbersWithout0And1[j] = 0;
-                    }
-                }
-            }
-        }
-        //remove all 0s from the array
-        int[] primes = new int[counter];
-        for (int i = 0; i < counter; i++) {
-            primes[i] = numbersWithoutMultiples[i];
-        }
 
-        return primes;
-    }
-
+    /**
+     * main method to call the primesUpTo method and testmethod
+     * @param args
+     */
     public static void main(String[] args) {
         int[] primes = primesUpTo(100);
         for (int i = 0; i < primes.length; i++) {
             System.out.println(primes[i]);
         }
+        testPrimesUpTo();
     }
 
+    /**
+     * method to find all prime numbers in a given range
+     *          - if the range is empty, return null
+     *          - if the range is not empty, return the prime numbers in the range
+     * @param n the range
+     * @return an array with all prime numbers
+     */
+    private static int[] primesUpTo(int n) {
+        //check if n is a valid number
+        if (n < 0) {
+            System.out.println("n must be greater than 0");
+            return null;
+        } else if (n == 0 || n == 1) {
+            return new int[0];
+        } else {
+            //list all numbers up to n in array
+            int[] numbers = new int[n + 1];
+            for (int i = 0; i <= n; i++) {
+                numbers[i] = i;
+            }
+            //remove 0 and 1 from the array
+            int[] numbersWithout0And1 = new int[n - 2];
+            for (int i = 0; i < n - 2; i++) {
+                numbersWithout0And1[i] = numbers[i + 2];
+            }
+            //remove all multiples of all numbers up to sqrt(n)+2 from the array
+            // by replacing all multiples with 0
+            int[] numbersWithoutMultiples = new int[n - 2];
+            int counter = 0;
+            for (int i = 0; i < n - 2; i++) {
+                if (numbersWithout0And1[i] != 0) {
+                    numbersWithoutMultiples[counter] = numbersWithout0And1[i];
+                    counter++;
+                    for (int j = i + 1; j < n - 2; j++) {
+                        if (numbersWithout0And1[j] % numbersWithout0And1[i] == 0) {
+                            numbersWithout0And1[j] = 0;
+                        }
+                    }
+                }
+            }
+            //remove all 0s from the array
+            int[] primes = new int[counter];
+            for (int i = 0; i < counter; i++) {
+                primes[i] = numbersWithoutMultiples[i];
+            }
+
+            return primes;
+        }
+    }
+
+    /**
+     * method to test the primesUpTo method
+     * @return true if the method works, false if not
+     */
     static void testPrimesUpTo(){
         int[] primes = primesUpTo(100);
         int[] primesExpected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
         if (primes.length != primesExpected.length) {
-            System.out.println("testPrimesUpTo failed: length of primes is not as expected");
+            System.out.println("testPrimesUpTo 100 failed: length of primes is not as expected");
             return;
         }
         for (int i = 0; i < primes.length; i++) {
             if (primes[i] != primesExpected[i]) {
-                System.out.println("testPrimesUpTo failed: primes is not as expected");
+                System.out.println("testPrimesUpTo 100 failed: primes is not as expected");
                 return;
             }
         }
-        System.out.println("testPrimesUpTo passed");
+        System.out.println("testPrimesUpTo 100 passed");
+
+        System.out.println("========================================");
+        int[] primes2 = primesUpTo(0);
+        if (primes2.length != 0) {
+            System.out.println("testPrimesUpTo 0 failed: length of primes is not as expected");
+            return;
+        }
+        System.out.println("testPrimesUpTo 0 passed");
+
+        System.out.println("========================================");
+        int[] primes3 = primesUpTo(1);
+        if (primes3.length != 0) {
+            System.out.println("testPrimesUpTo 1 failed: length of primes is not as expected");
+            return;
+        }
+        System.out.println("testPrimesUpTo 1 passed");
+
+        System.out.println("========================================");
+        int[] primes4 = primesUpTo(-1);
+        if (primes4 != null) {
+            System.out.println("testPrimesUpTo -1 failed: primes is not as expected");
+            return;
+        }
+        System.out.println("testPrimesUpTo -1 passed");
+
+        System.out.println("========================================");
+        int[] primes5 = primesUpTo(50);
+        int[] primesExpected5 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
+        if (primes5.length != primesExpected5.length) {
+            System.out.println("testPrimesUpTo 50 failed: length of primes is not as expected");
+            return;
+        }
+        for (int i = 0; i < primes5.length; i++) {
+            if (primes5[i] != primesExpected5[i]) {
+                System.out.println("testPrimesUpTo 50 failed: primes is not as expected");
+                return;
+            }
+        }
+        System.out.println("testPrimesUpTo 50 passed");
+
     }
 }
